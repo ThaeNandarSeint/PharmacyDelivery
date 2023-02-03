a1008846f1f44abae92a21f3450314c832c59ee3
hhtetwei <hhtet263510@gmail.com>
2023-02-03 13:31:56 +0630

    Removing auth in order

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,20 +1,35 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 // controllers
-const { createOrder, getAllOrders, getByOrderId, approveOrder, cancelOrder, deliverOrder } = require('../controllers/orderCtrl');
+const {
+    createOrder,
+    getAllOrders,
+    getByOrderId,
+    approveOrder,
+    cancelOrder,
+    deliverOrder,
+} = require("../controllers/orderCtrl");
 
 // middlewares
-const { roleAuth } = require('../middlewares/roleAuth');
+const { roleAuth } = require("../middlewares/roleAuth");
 
 // routes
-router.post('/', roleAuth("Superadmin", "Admin", "Supervisor", "Operator"), createOrder)
-router.put('/approve/:id', roleAuth("Superadmin", "Admin", "Supervisor", "Operator"), approveOrder)
-router.put('/deliver/:id', roleAuth("Superadmin", "Admin", "Supervisor", "Operator", "DeliveryBoy"), deliverOrder)
+router.post("/", createOrder);
+router.put(
+    "/approve/:id",
+    roleAuth("Superadmin", "Admin", "Supervisor", "Operator"),
+    approveOrder
+);
+router.put(
+    "/deliver/:id",
+    roleAuth("Superadmin", "Admin", "Supervisor", "Operator", "DeliveryBoy"),
+    deliverOrder
+);
 
 // can do all users
-router.get('/', getAllOrders)
-router.get('/orderId/:id', getByOrderId)
+router.get("/", getAllOrders);
+router.get("/orderId/:id", getByOrderId);
 
-router.put('/cancel/:id', cancelOrder)
+router.put("/cancel/:id", cancelOrder);
 
 module.exports = router;
