a91d95f79240431156ccf33b8baf9af486bd6120
ThaeNandarSeint <dardar2003nandar@gmail.com>
2023-02-17 21:19:14 +0630

    solve merge conflict

--- routes/deliveryPerson.route.js
+++ routes/deliveryPerson.route.js
@@ -4,7 +4,7 @@ const { getAllDeliveryPersons, createDeliveryPerson } = require('../controllers/
 // middlewares
 const { roleAuth } = require('../middlewares/roleAuth');
 
-router.post('/', createDeliveryPerson)
-router.get('/', getAllDeliveryPersons)
+router.post('/', roleAuth("Superadmin", "Admin"), createDeliveryPerson)
+router.get('/', roleAuth("Superadmin", "Admin"), getAllDeliveryPersons)
 
 module.exports = router;
