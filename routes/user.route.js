e312433f23a055aafe8f72a473eb3c1660551651
ThaeNandarSeint <dardar2003nandar@gmail.com>
2023-03-14 09:50:31 +0630

    remove roleAuth in getAllUsers route

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,17 +6,16 @@ const { auth } = require('../middlewares/auth');
 
 // middlewares
 const { roleAuth } = require('../middlewares/roleAuth');
-const { userAuth } = require('../middlewares/userAuth');
 
 const profileUpdateValidator = require('../validators/users/profileUpdate.validator');
 
 // routes
 router.get('/me', auth, getMyInfo)
-router.put('/me/password', userAuth, updatePassword)
-router.put('/me', userAuth, profileUpdateValidator, updateMe)
+router.put('/me/password', auth, updatePassword)
+router.put('/me', auth, profileUpdateValidator, updateMe)
 
-router.get('/', auth, roleAuth("Superadmin", "Admin"), getAllUsers)
-router.get('/:id', auth, roleAuth("Superadmin", "Admin"), getByUserId)
+router.get('/', auth, getAllUsers)
+router.get('/:id', auth, getByUserId)
 
 // ----------------------- can do only SuperAdmin & Admin -------------------------------
 
@@ -24,6 +23,6 @@ router.get('/:id', auth, roleAuth("Superadmin", "Admin"), getByUserId)
 
 // ----------------------- can do only Super Admin -------------------------------
 
-router.put('/grant/:id', roleAuth("Superadmin"), grantRole)
+router.put('/grant/:id', auth, roleAuth("Superadmin"), grantRole)
 
 module.exports = router;
